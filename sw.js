1a76dfaf580c393f2ab8ca6b4408aacdbe2ebfde
ysc <2978790137@qq.com>
2019-02-20 10:34:35 +0800

    仿照修改测试

--- sw.js
+++ sw.js
@@ -1,10 +1,44 @@
 
-importScripts('sw-toolbox.js');
+// importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html","style/style.css"]);
+// toolbox.precache(["index.html","style/style.css"]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+// toolbox.router.get('/images/*', toolbox.cacheFirst);
 
-toolbox.router.get('/*', toolbox.networkFirst, {
-  networkTimeoutSeconds: 5
+// toolbox.router.get('/*', toolbox.networkFirst, {
+//   networkTimeoutSeconds: 5
+// });
+
+// use a cacheName for cache versioning
+var cacheName = 'v1:static';
+
+// during the install phase you usually want to cache static assets
+self.addEventListener('install', function(e) {
+    // once the SW is installed, go ahead and fetch the resources to make this work offline
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+                './style/style.css',
+                './index.html'
+            ]).then(function() {
+                self.skipWaiting();
+            });
+        })
+    );
+});
+
+// when the browser fetches a url
+self.addEventListener('fetch', function(event) {
+    // either respond with the cached object or go ahead and fetch the actual url
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
 });
